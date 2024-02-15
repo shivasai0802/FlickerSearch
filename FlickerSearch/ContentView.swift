84be20a0eddba8f142a863034c9db5367484ac1e
shivasai0802 <33633472+shivasai0802@users.noreply.github.com>
2024-02-15 16:44:05 -0500

    Initial Commit

---
+++ FlickerSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlickerSearch
+//
+//  Created by shiva on 2/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlickerSearch/FlickerSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickerSearchApp.swift
+//  FlickerSearch
+//
+//  Created by shiva on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickerSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
